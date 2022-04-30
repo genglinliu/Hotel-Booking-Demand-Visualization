2379985ca7001288632e0a1c2d97752dc127e4c3
Genglin Liu <44108482+genglinliu@users.noreply.github.com>
2022-04-30 01:02:07 -0400

    adding donut chart example with anychart

--- dashboard/frontend/script.js
+++ dashboard/frontend/script.js
@@ -102,3 +102,33 @@ window.addEventListener('load', function () {
         }
     });
 })
+
+
+// // donut chart with chart.js
+anychart.onDocumentReady(function () {
+
+  // create data
+  var data = [
+    {x: "A", value: 637166},
+    {x: "B", value: 721630},
+    {x: "C", value: 148662},
+    {x: "D", value: 78662},
+    {x: "E", value: 90000}
+  ];
+
+  // create a pie chart and set the data
+  var chart = anychart.pie(data);
+
+  /* set the inner radius
+  (to turn the pie chart into a doughnut chart)*/
+  chart.innerRadius("30%");
+
+  // set the chart title
+  chart.title("Doughnut Chart: Basic Sample");
+
+  // set the container id
+  chart.container("container");
+
+  // initiate drawing the chart
+  chart.draw();
+});
