31e5e1b85d4d117eaef70131a32cadcddf5714b3
Genglin Liu <44108482+genglinliu@users.noreply.github.com>
2022-05-02 23:02:33 -0400

    update meal type donut and navbar

--- dashboard/frontend/script.js
+++ dashboard/frontend/script.js
@@ -27,29 +27,34 @@ anychart.onDocumentReady(function () {
 
   // create data
 
-  // 0,City Hotel,No Deposit,66442
-  // 1,City Hotel,Non Refund,12868
-  // 2,City Hotel,Refundable,20
-  // 3,Resort Hotel,No Deposit,38199
-  // 4,Resort Hotel,Non Refund,1719
-  // 5,Resort Hotel,Refundable,142
+  // 0  City Hotel	BB	62305
+  // 1	City Hotel	SC	10564
+  // 2	City Hotel	HB	6417
+  // 3	City Hotel	FB	44
+  // 4	Resort Hotel	BB	30005
+  // 5	Resort Hotel	HB	8046
+  // 6	Resort Hotel	Undefined	1169
+  // 7	Resort Hotel	FB	754
+  // 8	Resort Hotel	SC	86
   var data_city = [
-    {x: "No Deposit", value: 66442},
-    {x: "Non Refund", value: 12868},
-    {x: "Refundable", value: 20}
+    {x: "Bed & Breakfast", value: 62305},
+    {x: "no meal package", value: 10564},
+    {x: "breakfast and one other meal", value: 6417},
+    {x: "breakfast, lunch and dinner", value: 44}
   ];
 
   var data_resort = [
-    {x: "No Deposit", value: 38199},
-    {x: "Non Refund", value: 1719},
-    {x: "Refundable", value: 142}
+    {x: "Bed & Breakfast", value: 30005},
+    {x: "no meal package", value: 1255},
+    {x: "breakfast and one other meal", value: 8046},
+    {x: "breakfast, lunch and dinner", value: 754}
   ];
 
   // create a pie chart and set the data
   var chart = anychart.pie(data_city);
   // set the inner radius to turn the pie chart into a doughnut chart
   chart.innerRadius("50%");
-  chart.title("City Hotel Deposit Type Distribution");
+  chart.title("City Hotel Meal Type Distribution");
   chart.container("donut");
   chart.draw();
 
@@ -62,12 +67,12 @@ anychart.onDocumentReady(function () {
       let val = this.value; // this == the clicked radio,
       if (val == "city") {
         console.log("city")
-        chart.title("City Hotel Deposit Type Distribution");
+        chart.title("City Hotel Meal Type Distribution");
         chart.data(data_city);
       } 
       else if (val === "resort") {
         console.log("resort")
-        chart.title("Resort Hotel Deposit Type Distribution");
+        chart.title("Resort Hotel Meal Type Distribution");
         chart.data(data_resort);
       }
       chart.innerRadius("50%");
